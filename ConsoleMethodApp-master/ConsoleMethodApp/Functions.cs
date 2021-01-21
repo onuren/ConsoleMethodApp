795e9f8150f35058011b1ce1d8cd2d1a15ac05be
Onur Engün <47079490+onuren@users.noreply.github.com>
2021-01-21 03:59:24 +0300

    Add files via upload

---
+++ ConsoleMethodApp-master/ConsoleMethodApp/Functions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMethodApp
+{
+    class Functions
+    {
+        public Product AddProduct()
+        {
+            Product p = new Product();
+            Console.Write("|    Ürün kodunu yazınız: ");
+            p.id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("|       Ürün adı giriniz: ");
+            p.name = Console.ReadLine();
+            Console.Write("|Ürünün fiyatını giriniz: ");
+            p.price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("|     Ürün adedi yazınız: ");
+            p.stock = Convert.ToInt32(Console.ReadLine());
+
+            return p;
+        }
+        public void ListProducts(Product[] p)
+        {
+            foreach (var pro in p)
+            {
+                Console.WriteLine(pro.id);
+                Console.WriteLine(pro.name);
+                Console.WriteLine(pro.price+" TL");
+                Console.WriteLine(pro.stock+" ADT");
+                Console.WriteLine("***********************************");
+            }
+        }
+    }
+
+    class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ ConsoleMethodApp-master/ConsoleMethodApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choise = 0;
+            Functions func = new Functions();
+            Product[] productArray = new Product[0];
+
+            while (choise != 5)
+            {
+                Console.WriteLine("\n" +
+                  "         |*******************************************|\n" +
+                  "         | Lütfen yapmak istediğiniz işlemi seçiniz. |\n" +
+                  "         | 1- Ürün ekle.                             |\n" +
+                  "         | 2- Ürün sil.                              |\n" +
+                  "         | 3- Ürünleri listele.                      |\n" +
+                  "         | 4- Ürünü düzenle.                         |\n" +
+                  "         | 5- Çıkış.                                 |\n" +
+                  "         |*******************************************|\n");
+
+                choise = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (choise)
+                {
+                    case 1:
+                        Product[] temp = new Product[productArray.Length + 1];
+                        Array.Copy(productArray, temp, productArray.Length);
+                        Product p = func.AddProduct();
+                        temp[temp.Length - 1] = p;
+                        productArray = temp;
+                        break;
+                    case 2:
+                        Console.WriteLine("Silinecek ürün numarasını giriniz:");
+                        int numToRemove = Convert.ToInt32(Console.ReadLine());
+                        productArray = productArray.Where(val => val.id != numToRemove).ToArray();
+                        
+                        break;
+                    case 3:
+                        func.ListProducts(productArray);
+                        break;
+                    case 4:
+                        Console.WriteLine("Düzenlenecek ürün numarasını giriniz:");
+                        int numToEdit = Convert.ToInt32(Console.ReadLine());
+                        int indexEdit = 0;
+                        foreach (var product in productArray )
+                        {  
+                            if (product.id == numToEdit)
+                            {
+                                break;
+                            }
+                            indexEdit = indexEdit + 1;
+                        }
+                        Console.Write("|    Ürün kodunu yazınız: ");
+                        productArray[indexEdit].id = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("|       Ürün adı giriniz: ");
+                        productArray[indexEdit].name = Console.ReadLine();
+                        Console.Write("|Ürünün fiyatını giriniz: ");
+                        productArray[indexEdit].price = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("|     Ürün adedi yazınız: ");
+                        productArray[indexEdit].stock = Convert.ToInt32(Console.ReadLine());
+                        
+                        break;
+                }
+            }
+        }
+    }
+}
